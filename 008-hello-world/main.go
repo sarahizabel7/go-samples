a0ba99ee12538379e8edf25137e0650b3c8e8ead
sarah <sarahizabelp@gmail.com>
2019-05-05 15:23:14 -0300

    hello world

---
+++ 008-hello-world/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello everyone.")
+
+	foo()
+
+	fmt.Println("Something more.")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo.")
+}
+
+func bar() {
+	fmt.Println("And then we exited.")
+}
